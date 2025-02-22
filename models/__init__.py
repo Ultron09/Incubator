15ad8f42fac5feb9899b2e3dd67cc0286e8476c7
Suryaansh Prithvijit Singh <connect.singha@gmail.com>
2025-02-23 01:51:07 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 from .granite_model import (
     call_gemini_api,
     chat_with_ai,
-    assign_deadlines,
     mark_task_completed,
     assign_task_to_member,
     predict_business_growth
@@ -10,7 +9,6 @@ from .granite_model import (
 __all__ = [
     "call_gemini_api",
     "chat_with_ai",
-    "assign_deadlines",
     "mark_task_completed",
     "assign_task_to_member",
     "predict_business_growth"
