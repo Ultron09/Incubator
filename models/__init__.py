45d53d57769f3f178aa38aecf20868e8a9d1c10e
Suryaansh Prithvijit Singh <connect.singha@gmail.com>
2025-02-22 17:29:27 +0530

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,7 @@
-# models/__init__.py
-from .granite_model import get_granite_response
+from .granite_model import chat_with_ai
+from .granite_model import generate_business_checklist
+from .granite_model import assign_deadlines
+from .granite_model import mark_task_completed
+from .granite_model import assign_task_to_member
+from .granite_model import predict_business_growth
+from .granite_model import plot_business_growth_prediction
