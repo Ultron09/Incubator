95bd9170bbdd8db45aa2937fdf07726f4e903417
Suryaansh Prithvijit Singh <connect.singha@gmail.com>
2025-02-23 01:07:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-GEMINI_API_KEY = os.getenv("GeminiKey")
-GEMINI_API_URL = "https://your-gemini-api-endpoint-url"
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=GEMINI_API_KEY"
