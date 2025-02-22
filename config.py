7911616b48ca885e52951faa1791c9b779fc6de6
Suryaansh Prithvijit Singh <connect.singha@gmail.com>
2025-02-23 00:57:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
 
-IBM_GRANITE_API_KEY = os.getenv("IBM_GRANITE_API_KEY")
-IBM_GRANITE_URL = os.getenv("IBM_GRANITE_URL")
-IBM_WML_PROJECT_ID = os.getenv("IBM_WML_PROJECT_ID")  # Add this line
+GEMINI_API_KEY = os.getenv("GeminiKey")
+GEMINI_API_URL = "https://your-gemini-api-endpoint-url"
