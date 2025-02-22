465267cbc0f7bdf56a81afc89f5db17976a4eaf3
Suryaansh Prithvijit Singh <connect.singha@gmail.com>
2025-02-22 14:00:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,3 @@ load_dotenv()
 
 IBM_GRANITE_API_KEY = os.getenv("IBM_GRANITE_API_KEY")
 GOOGLE_GEMINI_API_KEY = os.getenv("GOOGLE_GEMINI_API_KEY")
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///progress.db")
